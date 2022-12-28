3fd409561f2c7fa3a2900ac267fddd5d47cfc311
Mario Yordanov <mario.yordanov@gtmhub.com>
2022-12-28 13:05:45 +0200

    add pipe

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const config = {
     default: 'arcade',
     arcade: {
       debug: true,
-      gravity: { y: 400, }
     }
   },
   scene: {
@@ -22,18 +21,23 @@ const VELOCITY = 200;
 const INITIAL_BIRD_POSITION = { x: config.width / 10, y: config.height / 2 }
 
 let bird = null;
-let flapVelocity = 150;
+let upperPipe = null;
+let lowerPipe = null;
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
 }
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
 
-  // config bird
   bird = this.physics.add.sprite(INITIAL_BIRD_POSITION.x, INITIAL_BIRD_POSITION.y, 'bird').setOrigin(0);
+  bird.body.gravity.y = 400;
+
+  upperPipe = this.physics.add.sprite(400, 100, 'pipe').setOrigin(0, 1);
+  lowerPipe = this.physics.add.sprite(400, (upperPipe.y + 100), 'pipe').setOrigin(0, 0);
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown_SPACE', flap);
