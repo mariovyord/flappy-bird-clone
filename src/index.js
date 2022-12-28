2e4991addb89af62ffaee3ae0f271508674e7fb6
Mario Yordanov <mario.yordanov@gtmhub.com>
2022-12-28 12:39:19 +0200

    add flapping

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ const config = {
   height: 600,
   physics: {
     default: 'arcade',
-    // arcade: {
-    //   gravity: { y: 200, }
-    // }
+    arcade: {
+      debug: true,
+      gravity: { y: 400, }
+    }
   },
   scene: {
     preload,
@@ -17,9 +18,11 @@ const config = {
   }
 }
 
-let bird = null;
-let totalDelta = 0;
 const VELOCITY = 200;
+
+let bird = null;
+let flapVelocity = 150;
+let totalDelta = 0;
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
@@ -29,16 +32,19 @@ function preload() {
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
 
+  // config bird
   bird = this.physics.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
-  bird.body.velocity.x = VELOCITY;
+
+  this.input.on('pointerdown', flap);
+  this.input.keyboard.on('keydown_SPACE', flap);
 }
 
 function update(time, delta) {
-  if (bird.x >= config.width - bird.width) {
-    bird.body.velocity.x = -VELOCITY;
-  } else if (bird.x <= 0) {
-    bird.body.velocity.x = VELOCITY;
-  }
+  
+}
+
+function flap() {
+  bird.body.velocity.y = -VELOCITY;
 }
 
 new Phaser.Game(config);
