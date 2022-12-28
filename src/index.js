79488c0392958ac255e239808addf28c9bbdd2a1
Mario Yordanov <mario.yordanov@gtmhub.com>
2022-12-28 12:31:00 +0200

    add moving bird

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import Phaser from "phaser";
 
 const config = {
@@ -7,22 +6,39 @@ const config = {
   height: 600,
   physics: {
     default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
+    // arcade: {
+    //   gravity: { y: 200, }
+    // }
   },
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create,
+    update
   }
-};
+}
+
+let bird = null;
+let totalDelta = 0;
+const VELOCITY = 200;
+
+function preload() {
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
+}
+
+function create() {
+  this.add.image(0, 0, 'sky').setOrigin(0, 0);
+
+  bird = this.physics.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
+  bird.body.velocity.x = VELOCITY;
+}
+
+function update(time, delta) {
+  if (bird.x >= config.width - bird.width) {
+    bird.body.velocity.x = -VELOCITY;
+  } else if (bird.x <= 0) {
+    bird.body.velocity.x = VELOCITY;
+  }
+}
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
