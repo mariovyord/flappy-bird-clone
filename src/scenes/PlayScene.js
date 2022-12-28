9646d80d73b9791287e1bd299237710e076d0b6a
Mario Yordanov <mario.yordanov@gtmhub.com>
2022-12-28 15:20:36 +0200

    add basic collider

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -24,33 +24,55 @@ export class PlayScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.image(0, 0, 'sky').setOrigin(0);
-    this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird').setOrigin(0);
-    this.bird.body.gravity.y = 400;
-
-    this.pipes = this.physics.add.group();
-
-    for (let i = 0; i < PIPES_TO_RENDER; i++) {
-      const upperPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 1);
-      const lowerPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 0);
-
-      this.placePipe(upperPipe, lowerPipe)
-    }
-
-    this.pipes.setVelocityX(-200);
-
-    this.input.on('pointerdown', this.flap, this);
-    this.input.keyboard.on('keydown_SPACE', this.flap, this);
+    this.createBackground();
+    this.createBird();
+    this.createPipes();
+    this.createColliders();
+    this.handleInputs();
   }
 
   update() {
-    if (this.bird.y > this.config.height || this.bird.y < -this.bird.height) {
-      this.restartBirdPosition();
-    }
-
+    this.checkGameStatus();
     this.recyclePipes();
   }
 
+  createBackground() {
+    this.add.image(0, 0, 'sky').setOrigin(0);
+  }
+
+    createBird() {
+        this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird').setOrigin(0);
+        this.bird.body.gravity.y = 400;
+    }  
+
+    createPipes() {
+        this.pipes = this.physics.add.group();
+
+        for (let i = 0; i < PIPES_TO_RENDER; i++) {
+        const upperPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 1);
+        const lowerPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 0);
+
+        this.placePipe(upperPipe, lowerPipe)
+        }
+
+        this.pipes.setVelocityX(-200);
+    }
+
+    createColliders() {
+        this.physics.add.collider(this.bird, this.pipes, this.gameOver, null, this);
+    }
+
+    handleInputs() {
+        this.input.on('pointerdown', this.flap, this);
+        this.input.keyboard.on('keydown_SPACE', this.flap, this);
+    }
+
+    checkGameStatus() {
+       if (this.bird.y > this.config.height || this.bird.y < -this.bird.height) {
+        this.gameOver();
+        }
+    }
+  
   placePipe(upperPipe, lowerPipe) {
     const rightMostX = this.getRightMostPipe();
     const pipeVerticalDistance = Phaser.Math.Between(...this.pipeVerticalDistanceRange);
@@ -84,7 +106,7 @@ export class PlayScene extends Phaser.Scene {
     return rightMostX;
   }
 
-  restartBirdPosition() {
+  gameOver() {
     this.bird.x = this.config.startPosition.x;
     this.bird.y = this.config.startPosition.y;
     this.bird.body.velocity.y = 0;
